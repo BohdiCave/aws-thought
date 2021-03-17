7cf30cc581b0e6c78a9d4ab262e3abfa2fadfebe
Bohdan Pechenyak <bohdan.pechenyak@gmail.com>
2021-03-16 22:49:04 -0400

    adding image-upload functionality

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 const ThoughtForm = () => {
   const [formState, setFormState] = useState({ username: '', thought: '' });
   const [characterCount, setCharacterCount] = useState(0);
+  const fileInput = useRef(null);
 
   // update state based on form input changes
   const handleChange = event => {
@@ -34,6 +35,30 @@ const ThoughtForm = () => {
     setCharacterCount(0);
   };
 
+  const handleImageUpload = event => {
+    event.preventDefault();
+    const data = new FormData();
+    data.append('image', fileInput.current.files[0]);
+    // send image file to endpoint with the postImage function
+    const postImage = async () => {
+      try {
+        const res = await fetch('/api/image-upload', {
+          mode: 'cors',
+          method: 'POST',
+          body: data
+        })
+        if (!res.ok) throw new Error(res.statusText);
+        const postResponse = await res.json();
+        setFormState({...formState, image: postResponse.Location})
+        console.log("postImage: ", postResponse.Location);
+        return postResponse.Location;
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    postImage();
+  };
+
   return (
     <div>
       <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
@@ -58,6 +83,21 @@ const ThoughtForm = () => {
           className="form-input col-12 col-md-9"
           onChange={handleChange}
         ></textarea>
+        <label className="form-input col-12  p-1">
+          Add an image to your thought: 
+          <input
+            type="file"
+            ref={fileInput}
+            className="form-input p-2"
+          />
+          <button 
+            className="btn" 
+            onClick={handleImageUpload} 
+            type="submit"
+          >
+            Upload
+          </button>
+        </label>
         <button className="btn col-12 col-md-3" type="submit">
           Submit
           </button>
